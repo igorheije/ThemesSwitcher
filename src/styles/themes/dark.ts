8325a00f5128cb48b7c6bf1ac807c48570036b86
igorheije <heije.tomori2014@outlook.com>
2021-01-11 20:17:28 -0300

    first

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default{
+    title: 'dark',
+
+    colors: {
+        primary: '#210B2C',
+        secundary: '#55286F',
+        
+        background: '#222',
+        text: '#fff'
+    },
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default{
+    title: 'light',
+
+    colors: {
+        primary: '#55286F',
+        secundary: '#210B2C',
+
+        background: '#f5f5f5',
+        text: '#333'
+    },
+}
